bf088b9d29ce36eaaece3eb1ec568c8db9e10b13
Vojtech Horky <vojtech.horky@gmail.com>
2010-06-10 08:31:52 +0200

    Add documentation to the Generator module

--- lib/Chemistry/Chempost/Generator.pm
+++ lib/Chemistry/Chempost/Generator.pm
@@ -2,6 +2,9 @@
 # Single molecule formula generator.
 package Generator;
 
+## @method new()
+# Constructor.
+#
 sub new {
 	my ( $self ) = @_;
 	my $this = { };
@@ -10,7 +13,10 @@ sub new {
 	return $this;
 }
 
-
+## @method void setIds(@ids)
+# Set used node ids.
+# @param @ids List of ids.
+#
 sub setIds {
 	my $this = shift @_;
 	my @nodeIds = @_;
@@ -25,6 +31,11 @@ sub setIds {
 	}
 }
 
+## @method int _bondNumber(string $kind)
+# Converts bond string to internal numeric representation.
+# When the @p $kind is not recognised, it is treated as a single bond.
+# @param $kind Bond kind.
+#
 sub _bondNumber {
 	my ( $this, $kind ) = @_;
 	if ($kind eq "single") {
@@ -42,6 +53,13 @@ sub _bondNumber {
 	}
 }
 
+## @method void setNodeInfo(int $id, string $captionLeft, string $captionMiddle, string $captionRight)
+# Sets node details.
+# @param $id Node id.
+# @param $captionLeft Left part of the node caption.
+# @param $captionMiddle Middle part of the node caption.
+# @param $captionRight Right part of the node caption.
+#
 sub setNodeInfo {
 	my ( $this, $id, $captionLeft, $captionMiddle, $captionRight ) = @_;
 
@@ -49,6 +67,13 @@ sub setNodeInfo {
 		= [ $captionLeft, $captionMiddle, $captionRight ];
 }
 
+## @method void addBond(int $from, int $to, int $type, int $angle)
+# Adds a bond between two nodes.
+# @param $from Starting node.
+# @param $to Target node.
+# @param $type Bond type.
+# @param $angle Bond angle (0 being at 3 o'clock).
+#
 sub addBond {
 	my ( $this, $from, $to, $type, $angle ) = @_;
 
@@ -60,6 +85,11 @@ sub addBond {
 	$this->{"nodes"}->{$to}->{"inverse-neighbours-count"}++;
 }
 
+## @method int _findFirstNode()
+# Finds possible first node to start drawing from.
+# @warning This method is not fully implemented and might end via call
+# to die() when true cycle is found.
+#
 sub _findFirstNode {
 	my ( $this ) = @_;
 
@@ -72,6 +102,11 @@ sub _findFirstNode {
 	die "No starting point found!";
 }
 
+## @method string _formatNodeCaption(int $nodeId)
+# Formats node caption for the MetaPost macro.
+# @param $nodeId Node id.
+# @return Quoted comma-separated list of strings.
+#
 sub _formatNodeCaption {
 	my ( $this, $nodeId ) = @_;
 
@@ -82,6 +117,10 @@ sub _formatNodeCaption {
 	);
 }
 
+## @method string _drawNode(int $current)
+# Recursively prepares MetaPost output for drawing a node.
+# @param $current Current node (the node to be drawn).
+#
 sub _drawNode {
 	my ( $this, $current ) =  ( @_ );
 	my $result = "";
@@ -107,6 +146,9 @@ sub _drawNode {
 	return $result;
 }
 
+## @method string generateMetaPost()
+# Generates MetaPost output.
+#
 sub generateMetaPost {
 	my ( $this ) = @_;
 
